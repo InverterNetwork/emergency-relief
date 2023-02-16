e1e0c50d32d718c683bc544f51857f6825989432
Oktay Şenkan <oktay.senkan@gmail.com>
2023-02-16 12:14:22 +0300

    fix: use env variable instead of hardcoded variable

--- src/configs/http.ts
+++ src/configs/http.ts
@@ -9,8 +9,7 @@ http.interceptors.request.use(
     const token = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
 
     if (token) {
-      config.headers.Authorization =
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkbWhmaXRxZXhjcnJyaXdtd3JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYzMTExMjMsImV4cCI6MTk5MTg4NzEyM30.s1-KhkkRAhF2tcl7dQ2Upj95kMoP1W6OE4g0JoM8SPU';
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     config.headers.platform = 'emergency_relief';
